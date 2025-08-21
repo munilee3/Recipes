a748ed28d23730d8eb442b351bbefc6a72accf63
muniswamy <muniswamykanike@gmail.com>
2025-08-21 20:15:21 +0530

    Final Submit

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const PORT = 4000;
 const app = express();
 
 app.use(cors({
-    origin: "https://recipes-frontend-8xvd.onrender.com",
+    origin: "https://recipes-mauve-psi.vercel.app/",
     methods: ['GET', 'POST'],
 }));
 
